39a735eb290adfbd9e6e68d526b6db13f50e1bae
Dmitriy Konoplev <konoplev@yarnord.ru>
2018-10-18 12:16:51 +0300

    Initial commit

---
+++ src/main/java/ru/sandbox/flowablesandbox/FlowableSandboxApplication.java
@@ -0,0 +1,12 @@
+package ru.sandbox.flowablesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableSandboxApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/sandbox/flowablesandbox/FlowableSandboxApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.sandbox.flowablesandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlowableSandboxApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
